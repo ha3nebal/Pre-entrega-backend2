ed2d9e22d341664b173df1734a6f548ccabeb87b
Anibal Allendes <anibalallendesaravena@gmail.com>
2026-08-26 11:50:43 -0400

    feat: configure JWT authentication

--- src/config/env.js
+++ src/config/env.js
@@ -6,5 +6,6 @@ export const config = {
     PORT: process.env.PORT || 8080,
     NODE_ENV: process.env.NODE_ENV,
     MONGO_URL: process.env.MONGO_URL,
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN
 };
