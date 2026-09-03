5cb77fa2c8e7bbc107d778a65b3b994cfd5b6be9
Anibal Allendes <anibalallendesaravena@gmail.com>
2026-09-03 13:16:10 -0400

    feat: implement session logout

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -1,6 +1,7 @@
 import {
     getSessionInfo,
-    registerUser
+    registerUser,
+    loginUser
 } from "../services/sessions.service.js";
 
 import { sendSuccess } from "../utils/response.js";
@@ -34,5 +35,59 @@ export const register = async (req, res, next) => {
         next(error);
 
     }
+
+};
+
+export const login = async (req, res, next) => {
+
+    try {
+
+        const { email, password } = req.body;
+
+        const token = await loginUser(
+            email,
+            password
+        );
+
+        res.cookie(
+            "currentUser",
+            token,
+            {
+                httpOnly: true,
+                sameSite: "lax",
+                maxAge: 3600000,
+                secure: process.env.NODE_ENV === "production"
+            }
+        );
+
+        res.status(200).json({
+            status: "success",
+            message: "Login correcto"
+        });
+
+    } catch (error) {
+
+        next(error);
+
+    }
+};
+
+export const currentUser = (req, res) => {
+
+    res.status(200).json({
+        status: "success",
+        payload: req.user
+    });
+
+};
+
+export const logout = (req, res) => {
+
+    res.clearCookie("currentUser");
+
+    res.status(200).json({
+        status: "success",
+        message: "Sesión cerrada"
+    });
 
 };
--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -2,13 +2,24 @@ import { Router } from "express";
 
 import {
     getSession,
-    register
+    register,
+    login,
+    currentUser,
+    logout
 } from "../controllers/sessions.controller.js";
+
+import { auth } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.get("/", getSession);
 
 router.post("/register", register);
+
+router.post("/login", login);
+
+router.get("/current", auth, currentUser);
+
+router.post("/logout", logout);
 
 export default router;
