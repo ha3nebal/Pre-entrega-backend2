e7a009295e182f88e0c6621f838810416df736c6
Anibal Allendes <anibalallendesaravena@gmail.com>
2026-09-14 20:16:37 -0300

    refactor: initialize Passport in app

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+
+import "./config/passport.config.js";
 
 import eventsRouter from "./routes/events.router.js";
 import sessionsRouter from "./routes/sessions.router.js";
@@ -6,27 +10,24 @@ import sessionsRouter from "./routes/sessions.router.js";
 import { notFound } from "./middlewares/notFound.middleware.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
-import cookieParser from "cookie-parser";
-
 const app = express();
 
 app.use(express.json());
+
 app.use(cookieParser());
 
-app.get("/api/health", (req, res) => {
+app.use(passport.initialize());
 
+app.get("/api/health", (req, res) => {
     res.status(200).json({
         status: "ok",
         message: "Servidor activo"
     });
-
 });
 
 app.use("/api/events", eventsRouter);
 
 app.use("/api/sessions", sessionsRouter);
-
-// Siempre al final
 
 app.use(notFound);
 
