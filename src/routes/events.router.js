8d8c627318d79915fe66b1512071bc74987e74b9
Anibal Allendes <anibalallendesaravena@gmail.com>
2026-09-15 19:37:52 -0300

    refactor: update event routes

--- src/routes/events.router.js
+++ src/routes/events.router.js
@@ -10,29 +10,20 @@ import {
 
 const router = Router();
 
-/**
- * Obtener todos los eventos
- */
+
 router.get("/", getEvents);
 
-/**
- * Obtener un evento por ID
- */
+
 router.get("/:id", getEvent);
 
-/**
- * Crear un nuevo evento
- */
+
 router.post("/", createNewEvent);
 
-/**
- * Actualizar un evento existente
- */
+
 router.put("/:id", updateExistingEvent);
 
-/**
- * Eliminar un evento
- */
+
 router.delete("/:id", deleteExistingEvent);
+
 
 export default router;
